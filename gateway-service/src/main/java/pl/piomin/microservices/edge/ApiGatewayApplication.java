dbaa3ca866aabfc9589e61451d20552745f440c0
Igor <merdoc97@gmail.com>
2018-05-22 13:24:48 +0300

    add hystrix dashboard to gateway

--- gateway-service/src/main/java/pl/piomin/microservices/edge/ApiGatewayApplication.java
+++ gateway-service/src/main/java/pl/piomin/microservices/edge/ApiGatewayApplication.java
@@ -4,23 +4,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaClient
 @EnableDiscoveryClient
+@EnableHystrix
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(ApiGatewayApplication.class).web(true).run(args);
-	}
-
-	@Bean
-	public AlwaysSampler defaultSampler() {
-	  return new AlwaysSampler();
 	}
 
 }
