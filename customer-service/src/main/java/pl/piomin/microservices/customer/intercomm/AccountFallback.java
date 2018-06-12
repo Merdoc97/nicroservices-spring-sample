82afb2dc7faafc6de0373281d0c986115b442002
Igor <merdoc97@gmail.com>
2018-06-12 17:41:45 +0300

    migration to boot 2.0 in progress

--- customer-service/src/main/java/pl/piomin/microservices/customer/intercomm/AccountFallback.java
+++ customer-service/src/main/java/pl/piomin/microservices/customer/intercomm/AccountFallback.java
@@ -1,18 +1,17 @@
 package pl.piomin.microservices.customer.intercomm;
 
+import org.springframework.stereotype.Component;
+import pl.piomin.microservices.customer.model.Account;
+
 import java.util.ArrayList;
 import java.util.List;
-
-import org.springframework.stereotype.Component;
-
-import pl.piomin.microservices.customer.model.Account;
 
 @Component
 public class AccountFallback implements AccountClient {
 
 	@Override
 	public List<Account> getAccounts(Integer customerId) {
-		List<Account> acc = new ArrayList<Account>();
+		List<Account> acc = new ArrayList<>();
 		return acc;
 	}
 	
