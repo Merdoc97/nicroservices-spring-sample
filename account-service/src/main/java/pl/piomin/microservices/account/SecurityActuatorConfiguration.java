12df27f587d4ae3692cc54137453805cc35df2a9
Igor <merdoc97@gmail.com>
2018-06-18 17:43:53 +0300

    add security to actuator and add hystrix dashboard to monitor turbine

--- account-service/src/main/java/pl/piomin/microservices/account/SecurityActuatorConfiguration.java
+++ account-service/src/main/java/pl/piomin/microservices/account/SecurityActuatorConfiguration.java
@@ -1,19 +1,33 @@
 package pl.piomin.microservices.account;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 /**
  */
+
 @Configuration
 public class SecurityActuatorConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Value("${security.user.name}")
+    private String userName;
+    @Value("${management.security.roles}")
+    private String userRole;
     @Value("${management.context-path}")
     private String actuatorEndpoint;
+    @Value("${security.user.password}")
+    private String userPassword;
+    @Value("${management.context-path}")
+    private String actuatorPath;
+    @Value("${management.security.roles}")
+    private String actuatorRole;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -22,5 +36,14 @@ public class SecurityActuatorConfiguration extends WebSecurityConfigurerAdapter 
                 .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                 .antMatchers("/**").permitAll();
+    }
+
+    //    in boot 2.0 is required
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                //    in boot 2.0 password encoder is required
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser(userName).password(userPassword).roles(userRole);
     }
 }
--- gateway-service/src/main/java/pl/piomin/microservices/edge/SecurityActuatorConfiguration.java
+++ gateway-service/src/main/java/pl/piomin/microservices/edge/SecurityActuatorConfiguration.java
@@ -1,26 +1,54 @@
 package pl.piomin.microservices.edge;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 /**
  */
+@EnableWebSecurity
 @Configuration
 public class SecurityActuatorConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Value("${security.user.name}")
+    private String userName;
+    @Value("${management.security.roles}")
+    private String userRole;
     @Value("${management.context-path}")
     private String actuatorEndpoint;
+    @Value("${security.user.password}")
+    private String userPassword;
+    @Value("${management.context-path}")
+    private String actuatorPath;
+    @Value("${management.security.roles}")
+    private String actuatorRole;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
+                .antMatchers(actuatorPath + "/**").hasAnyRole(actuatorRole)
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                .antMatchers("/**").permitAll();
+                .antMatchers("/**").permitAll()
+                .and()
+                .httpBasic().and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);;
     }
+
+//    in boot 2.0 is required
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+          auth.inMemoryAuthentication()
+                  //    in boot 2.0 password encoder is required
+                  .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser(userName).password(userPassword).roles(userRole);
+    }
+
 }
