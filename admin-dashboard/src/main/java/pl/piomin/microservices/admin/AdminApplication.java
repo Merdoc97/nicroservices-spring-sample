bd822d7ed6ea8b75eee51a4f77391ef0cdd88e17
merdoc97@gmail.com <merdoc97@gmail.com>
2018-12-30 21:16:12 +0200

    add examples xow to write zuul tests

--- admin-dashboard/src/main/java/pl/piomin/microservices/admin/AdminApplication.java
+++ admin-dashboard/src/main/java/pl/piomin/microservices/admin/AdminApplication.java
@@ -4,13 +4,13 @@ package pl.piomin.microservices.admin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableAdminServer
 public class AdminApplication {
 
