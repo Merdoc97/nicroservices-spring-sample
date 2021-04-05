449a084c8c67420e1ec2cf22ff34094d1659fd26
merdoc <merdoc97@gmail.com>
2021-04-05 17:52:46 +0300

    add jacoco coverage example

--- admin-dashboard/src/main/java/pl/piomin/microservices/admin/config/PrometheusConfiguration.java
+++ admin-dashboard/src/main/java/pl/piomin/microservices/admin/config/PrometheusConfiguration.java
@@ -3,11 +3,14 @@ package pl.piomin.microservices.admin.config;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.actuate.autoconfigure.metrics.export.prometheus.PrometheusMetricsExportAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class PrometheusConfig {
+@AutoConfigureAfter(value = {PrometheusMetricsExportAutoConfiguration.class})
+public class PrometheusConfiguration {
 
     @Bean
     MeterRegistryCustomizer<MeterRegistry> metricsCommonTags(@Value("${spring.application.name}") String applicationName) {
